76494d3f70b7282037c02925bdb829607ac5d741
AdjeteAdjevi <adjete.adjevi@ul.com>
2024-11-18 22:04:06 +0000

    Add project files.

---
+++ SpecFlowProjectTest/StepDefinitions/CalculatorStepDefinitions.cs
@@ -0,0 +1,35 @@
+namespace SpecFlowProjectTest.StepDefinitions
+{
+    [Binding]
+    public sealed class CalculatorStepDefinitions
+    {
+        // For additional details on SpecFlow step definitions see https://go.specflow.org/doc-stepdef
+        private int num1;
+        private int num2;
+        private int total;
+
+        [Given("the first number is (.*)")]
+        public void GivenTheFirstNumberIs(int number)
+        {
+            num1 = number;
+        }
+
+        [Given("the second number is (.*)")]
+        public void GivenTheSecondNumberIs(int number)
+        {
+            num2 = number;
+        }
+
+        [When("the two numbers are added")]
+        public void WhenTheTwoNumbersAreAdded()
+        {
+            total = num1 + num2;
+        }
+
+        [Then("the result should be (.*)")]
+        public void ThenTheResultShouldBe(int result)
+        {
+            total.Should().Be(result);
+        }
+    }
+}
